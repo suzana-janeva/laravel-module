2a5c55330dd28d49ba48822d6e1a9ed4fdb4d13b
Suzana Janeva <suzana@codeart.mk>
2025-05-01 16:32:37 +0200

    updated  app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Service Provider Directory')</title>
-     @vite(['resources/css/app.css', 'resources/css/custom.css'])
+    @vite(['resources/js/app.js'])
 </head>
 <body class="bg-gray-100">
 
